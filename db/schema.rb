876e31943115cf6670af11699fc2e25adfc209e6
Mikey Couture <mcouture@deloitte.ca>
2021-07-22 14:47:01 -0400

    creating article controller and show file to render frontend

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_21_191633) do
+ActiveRecord::Schema.define(version: 2021_07_21_201401) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "description"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
 end
